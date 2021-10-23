2fc05c07a831982806ad02cc6e33a9114760a0a3
iamfikri001 <iamfikri001@gmail.com>
2021-10-23 19:24:01 +0700

    first commit

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    public $globalArr = ['kucinng', 'ayam', 'kambing'];
+
+    public function index()
+    {
+        foreach($this->globalArr as $val){
+            echo $val;
+            echo "\n";
+        }
+    }
+    
+    public function create(Request $request)
+    {
+        echo 'Data sebelum melakukan store data array';
+        echo "\n";
+
+        echo $this->index();
+        
+        echo "\n";
+        echo 'Data setelah store data array';
+        echo "\n";
+        array_push($this->globalArr, $request->name);
+        echo $this->index();
+    }
+
+    public function update(Request $request)
+    {
+        echo 'Data sebelum melakukan update data array';
+        echo "\n";
+
+        echo $this->index();
+        
+        echo "\n";
+        echo 'Data setelah update data array';
+        echo "\n";
+        $this->globalArr = array_replace($this->globalArr, array($request->index => $request->name));
+        echo $this->index();
+    }
+
+    public function delete(Request $request)
+    {
+        echo 'Data sebelum melakukan delete data array';
+        echo "\n";
+
+        echo $this->index();
+        
+        echo "\n";
+        echo 'Data setelah delete data array';
+        echo "\n";
+        unset($this->globalArr[$request->index]);
+        echo $this->index();
+    }
+}
